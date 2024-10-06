7a217fcc52fe940018f7e480ffcca24aaada53c5
Faizurcse <mdfaizurrahman2000@gmail.com>
2024-10-06 17:41:14 +0530

    cimiit

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -18,8 +18,8 @@ function Hero() {
             </motion.h1>
           </div>
           <div className="flexColStart hero-des">
-            <span className="secondaryText-2" >Theme<span style={{color:'orange' , fontSize:'1rem'}}> : </span>Empowering Minds</span>
-            <span className="secondaryText-2" >
+            <span style={{color:'orange'}} className="secondaryText-2" >Theme<span style={{ fontSize:'1rem'}}> : </span>Empowering Minds</span>
+            <span style={{color:'orange'}} className="secondaryText-2" >
               The Evolution of Cognitive Technologies in Shaping the Future
             </span>
           </div>
